fc9c617181aafb81a3db833fbf042071d2a01ca4
trxrse12 <trxrse@gmail.com>
2021-04-01 10:46:53 +0100

    01/04/2021 H1. 1. Ready to implement components

--- src/message-store/read.js
+++ src/message-store/read.js
@@ -22,6 +22,7 @@ function createRead ({db = {}} = {}){
    * @returns {*|Promise<T | never>|undefined}
    */
   function read(streamName, fromPosition = 0, maxMessages=1000) {
+    console.log('READING streamName = ', streamName, ' FROM POSITION ', fromPosition)
     let query = null;;
     let values = [];
     if (streamName.includes('-')){
