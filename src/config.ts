37188648c296b12264cef652aac8acf92bdac604
trxrse12 <trxrse@gmail.com>
2021-06-15 14:02:08 +0100

    15/06/2020 H3. env.js converted to ts, also started the conversion on config.ts

---
+++ src/config.ts
@@ -0,0 +1,119 @@
+// @ts-check
+
+import { knex, Knex } from 'knex';
+// Primitives
+
+import { ProcessEnv } from './env';
+
+type CtexConfig = {
+  db: Knex.Client;
+  [key: string]: any;
+};
+
+const createPickupTransport = require('nodemailer-pickup-transport');
+const createKnexClient = require('./knex-client');
+const createPostGresClient = require('./postgres-client');
+const createMessageStore = require('./message-store');
+
+const createHomePageAggregator = require('./aggregators/home-page');
+const createUserCredentialsAggregator = require('./aggregators/user-credentials');
+
+const createRegisterUsersApp = require('./app/register-users');
+const createHomeApp = require('./app/home');
+const createRecordViewingsApp = require('./app/record-viewings');
+const createAuthenticateApp = require('./app/authenticate');
+
+const createIdentityComponent = require('./components/identity');
+
+const createSendEmailComponent = require('./components/send-email');
+
+const createVideoPublishingComponent = require('./components/video-publishing');
+
+const createCreatorsPortal = require('./app/creators-portal');
+
+function createConfig({
+  env: {
+    databaseUrl,
+    emailDirectory,
+    messageStoreConnectionString,
+    systemSenderEmailAddress,
+  },
+}: {
+  env: ProcessEnv;
+}): CtexConfig {
+  let knexClient: Knex.Client;
+  if (typeof databaseUrl === 'string') {
+    knexClient = createKnexClient({
+      connectionString: databaseUrl,
+    });
+    const postgresClient = createPostGresClient({
+      connectionString: messageStoreConnectionString,
+    });
+
+    const messageStore = createMessageStore({ db: postgresClient });
+
+    const homePageAggregator = createHomePageAggregator({
+      db: knexClient,
+      messageStore,
+    });
+    const userCredentialsAggregator = createUserCredentialsAggregator({
+      db: knexClient,
+      messageStore,
+    });
+    const aggregators = [homePageAggregator, userCredentialsAggregator];
+
+    const identityComponent = createIdentityComponent({ messageStore });
+    const transport = createPickupTransport({ directory: emailDirectory });
+    const sendEmailComponent = createSendEmailComponent({
+      messageStore,
+      systemSenderEmailAddress,
+      transport,
+    });
+    const videoPublishingComponent = createVideoPublishingComponent({
+      messageStore,
+    });
+    const components = [
+      identityComponent,
+      sendEmailComponent,
+      videoPublishingComponent,
+    ];
+
+    const homeApp = createHomeApp({ db: knexClient });
+    const registerUsersApp = createRegisterUsersApp({
+      db: knexClient,
+      messageStore,
+    });
+    const recordViewingsApp = createRecordViewingsApp({ messageStore });
+    const creatorsPortalApp = createCreatorsPortal({
+      db: knexClient,
+      messageStore,
+    });
+    const authenticateApp = createAuthenticateApp({
+      db: knexClient,
+      messageStore,
+    });
+
+    return {
+      db: knexClient,
+      messageStore,
+
+      homeApp,
+      recordViewingsApp,
+      registerUsersApp,
+      authenticateApp,
+      creatorsPortalApp,
+
+      homePageAggregator,
+      aggregators,
+
+      components,
+      identityComponent,
+      sendEmailComponent,
+      videoPublishingComponent,
+    };
+  }
+
+  throw new Error('createConfig() error: Invalid database URL');
+}
+
+module.exports = createConfig;
--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,14 @@
+// @ts-check
+
+import { DotenvConfigOutput } from 'dotenv';
+
 const colors = require('colors/safe');
 const dotenv = require('dotenv');
 
+// @ts-ignore
 const packageJson = require('../package.json');
 
-const envResult = dotenv.config();
+const envResult: DotenvConfigOutput = dotenv.config();
 
 if (envResult.error){
   console.error(
@@ -13,15 +18,25 @@ if (envResult.error){
   process.exit(1);
 }
 
+export interface ProcessEnv {
+  [key: string]: string | number | undefined;
+}
 
-function requireFromEnv(key){
-  if (!process.env[key]){
-    console.error(`${colors.red('[APP ERROR] Missing env variable:')} ${key}`)
+function requireFromEnv(key: string): ProcessEnv {
+  const processEnvKey: ProcessEnv = process.env[
+    key
+  ]?.toString() as unknown as ProcessEnv;
+
+  if (!processEnvKey) {
+    console.error(`${colors.red('[APP ERROR] Missing env variable:')} ${key}`);
 
     return process.exit(1);
   }
 
-  return process.env[key];
+  if (typeof processEnvKey === 'string') {
+    return processEnvKey;
+  }
+  throw new Error('ProcessEnv() error: invalid process.env key');
 }
 
 module.exports = {
@@ -29,7 +44,7 @@ module.exports = {
   cookieSecret: requireFromEnv('COOKIE_SECRET'),
   databaseUrl: requireFromEnv('DATABASE_URL'),
   env: requireFromEnv('NODE_ENV'),
-  port: parseInt(requireFromEnv('PORT'), 10),
+  port: Math.floor(Number(requireFromEnv('PORT'))),
   emailDirectory: requireFromEnv('EMAIL_DIRECTORY'),
   systemSenderEmailAddress: requireFromEnv('SYSTEM_SENDER_EMAIL_ADDRESS'),
   version: packageJson.version,
